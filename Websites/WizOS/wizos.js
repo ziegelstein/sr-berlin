0fcacda64d3c7e51360a7b24fe91b3eec54c26f5
Felix <Felix.Frings@yahoo.de>
2018-09-19 20:34:42 +0200

    Added first Website, a small riddle for Players. Bad CSS

---
+++ Websites/WizOS/wizos.js
@@ -0,0 +1,12 @@
+// wizos.js
+
+function pasuser(form) {
+if (form.id.value=="buddlerum") {
+if (form.pass.value=="12345") {
+location="login.html"
+} else {
+alert("Invalid Password")
+}
+} else {  alert("Get geeked - buddlerum")
+}
+}
